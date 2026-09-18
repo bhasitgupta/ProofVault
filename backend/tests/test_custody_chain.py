07280d64ed188c20b30d67160ec7bcb390f48d3f
bhasitgupta <bhasitgupta@gmail.com>
2026-09-18 19:42:14 +0530

    test(custody): add unit tests for unbroken chain continuity

---
+++ backend/tests/test_custody_chain.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestCustodyChain(unittest.TestCase):
+    def test_unbroken_chain(self):
+        events = [{'hash': '0x1', 'prev': '0x0'}, {'hash': '0x2', 'prev': '0x1'}]
+        self.assertEqual(events[1]['prev'], events[0]['hash'])
