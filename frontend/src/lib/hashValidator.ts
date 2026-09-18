d7d28f38f9b581a595ffc127ff5aea141ed819b1
bhasitgupta <bhasitgupta@gmail.com>
2026-09-18 19:41:35 +0530

    feat(utils): add Polygonscan Amoy address link resolver

--- frontend/src/lib/hashValidator.ts
+++ frontend/src/lib/hashValidator.ts
@@ -3,3 +3,5 @@ export const isSha256 = (str: string): boolean => /^[a-fA-F0-9]{64}$/.test(str);
 export const formatTruncatedHash = (h: string): string => h.length > 16 ? `${h.slice(0, 8)}...${h.slice(-8)}` : h;
 
 export const formatTxExplorerUrl = (tx: string): string => `https://amoy.polygonscan.com/tx/${tx}`;
+
+export const formatAddressUrl = (addr: string): string => `https://amoy.polygonscan.com/address/${addr}`;
